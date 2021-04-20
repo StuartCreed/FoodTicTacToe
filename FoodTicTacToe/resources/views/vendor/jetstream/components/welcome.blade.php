d6858167242108f39ccdcf36b98c6f9fbd1ca73e
Stuart Creed <stuart.a.creed@gmail.com>
2021-04-20 18:20:44 +0100

    feat(initial): Scaffolds back panels

--- FoodTicTacToe/resources/views/vendor/jetstream/components/welcome.blade.php
+++ FoodTicTacToe/resources/views/vendor/jetstream/components/welcome.blade.php
@@ -1,7 +1,1 @@
-<div class="p-6 sm:px-20 bg-white border-b border-gray-200">
 
-</div>
-
-<div class="bg-gray-200 bg-opacity-25 grid grid-cols-1 md:grid-cols-2">
-    {{-- INJECT VUE HERE--}}
-</div>
