be38ff71548f1da1dba5243c6ab9babb91413d61
Stuart Creed <stuart.a.creed@gmail.com>
2021-04-20 22:37:39 +0100

    feat(game): Board Structure

--- FoodTicTacToe/resources/js/components/game/Board.vue
+++ FoodTicTacToe/resources/js/components/game/Board.vue
@@ -1,5 +1,15 @@
 <template>
-    <div>Board</div>
+    <div class="board grid grid-cols-3">
+        <div data-cell-index="0" class="cell rounded-tl-xl">0</div>
+        <div data-cell-index="1" class="cell">1</div>
+        <div data-cell-index="2" class="cell rounded-tr-xl">2</div>
+        <div data-cell-index="3" class="cell">3</div>
+        <div data-cell-index="4" class="cell">4</div>
+        <div data-cell-index="5" class="cell">5</div>
+        <div data-cell-index="6" class="cell rounded-bl-xl">6</div>
+        <div data-cell-index="7" class="cell">7</div>
+        <div data-cell-index="8" class="cell rounded-br-xl">8</div>
+    </div>
 </template>
 
 <script>
@@ -10,4 +20,11 @@ export default {
 </script>
 
 <style scoped>
+    .cell {
+        display: flex;
+        border: 1px solid black;
+        padding: 50px;
+        justify-content: center;
+        align-items: center;
+    }
 </style>
