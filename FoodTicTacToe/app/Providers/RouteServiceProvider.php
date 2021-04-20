87f1caedf64669599842671b348788a4679c4d4e
Stuart Creed <stuart.a.creed@gmail.com>
2021-04-20 17:28:01 +0100

    feat(initial): Redirects to route Game after login

--- FoodTicTacToe/app/Providers/RouteServiceProvider.php
+++ FoodTicTacToe/app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/scoreboard';
+    public const HOME = '/game';
 
     /**
      * The controller namespace for the application.
