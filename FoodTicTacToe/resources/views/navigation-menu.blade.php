6a8b9c0745da6c3ed3b596e69568f3676a0a9bad
Stuart Creed <stuart.a.creed@gmail.com>
2021-04-20 19:32:11 +0100

    feat(initial): Navs to game on logo click

--- FoodTicTacToe/resources/views/navigation-menu.blade.php
+++ FoodTicTacToe/resources/views/navigation-menu.blade.php
@@ -5,7 +5,7 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="flex-shrink-0 flex items-center">
-                    <a href="{{ route('scoreboard') }}">
+                    <a href="{{ route('game') }}">
                         <x-jet-application-mark class="block h-9 w-auto" />
                     </a>
                 </div>
